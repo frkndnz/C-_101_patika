a34df97e14a47fc163d7464cc1e937e064753695
Furkan Deniz <dnzfurkan@outlook.com>
2021-05-28 15:47:40 +0300

    hata yonetımı

---
+++ hata_yonetımı/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace hata_yonetımı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          try
+          {
+                // int a =int.Parse(null);
+              //int a = int.Parse("test");
+              int ab = int.Parse("-2000000000000");
+
+          }
+          catch(ArgumentNullException ex)
+          {
+              Console.WriteLine("boş değer girdiniz");
+              Console.WriteLine(ex);
+          }
+          catch (FormatException ex)
+          {
+              
+              Console.WriteLine("veri tipi uygun değil");
+              Console.WriteLine(ex);
+          }
+          catch(OverflowException ex)
+          {
+              Console.WriteLine("çok küçük yada çok büyük değer girdiniz");
+              Console.WriteLine(ex);
+          }
+          finally
+          {
+              Console.WriteLine("işlem tamamlandı");
+          }
+
+        }
+    }
+}
